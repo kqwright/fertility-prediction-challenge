06eb5af8cbe3a5c0c8d0601b298a164af2165cb8
kqwright <kelsey.wright@helsinki.fi>
2024-05-31 21:11:02 +0300

    personality two

--- submission.R
+++ submission.R
@@ -136,7 +136,7 @@ clean_df <- function(df, background_df = NULL) {
   
   # Generate personality
   #df[is.na(df$cp20l026),]$cp20l026 <- "999"
-  df[is.na(df$cp20l023),]$cp20l023 <- "999"
+  #df[is.na(df$cp20l023),]$cp20l023 <- "999"
   df$personality <- "NA"
   df[(df$cp20l026==1 | df$cp20l026==2 | df$cp20l026==3) & (df$cp20l023==1 | df$cp20l023==2 | df$cp20l023==3),]$personality <- "less_social_less_stressed"
   df[(df$cp20l026==4 | df$cp20l026==5) & (df$cp20l023==1 | df$cp20l023==2 | df$cp20l023==3),]$personality <- "more_social_less_stressed"
