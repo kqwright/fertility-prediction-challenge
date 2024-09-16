12f06090e49d152eaa0b6e79b49c40e58adaf6d0
kqwright <kelsey.wright@helsinki.fi>
2024-09-16 10:53:49 +0300

    Update submission 2nd 9.16.24.R

--- submission.R
+++ submission.R
@@ -459,46 +459,13 @@ train_cleaned <- clean_df(train)
 train_save_model(train_cleaned, outcome)
 
 
-#####################
-
-#train_save_model <- function(cleaned_df, outcome_df) {
-  # Trains a model using the cleaned dataframe and saves the model to a file.
-  
-  # Parameters:
-  # cleaned_df (dataframe): The cleaned data from clean_df function to be used for training the model.
-  # outcome_df (dataframe): The data with the outcome variable (e.g., from PreFer_train_outcome.csv or PreFer_fake_outcome.csv).
-  
-  ## This script contains a bare minimum working example
- # set.seed(1) # not useful here because logistic regression deterministic
-  
-  # Combine cleaned_df and outcome_df
-  #model_df <- merge(cleaned_df, outcome_df, by = "nomem_encr")
-  
-  # Logistic regression model
-  #model <- glm(new_child ~ gender_bg*(age+migration+education+occupation+satisfied_own_finance+owner+income+rela_satisfied+health+relig+personality+urban+help_fr_parents+next_child+first_birth+partner_dur+marriage_dur+field_edu), family = "binomial", data = model_df) 
-  
-  # Save the model
-  #saveRDS(model, "model.rds")
-#}
-
-
-###############
-
-#setwd("Z:/PreFer/") 
-
-#train_cleaned <- clean_df(train)
-
-# training and saving the model
-#train_save_model(train_cleaned, outcome)
-
-
 ###############################
-#setwd("Z:/PreFer/other_data/") 
+setwd("Z:/PreFer/other_data/") 
 
 
-#fake <- data.table::fread("PreFer_fake_data.csv",
-#                          keepLeadingZeros = TRUE, # if FALSE adds zeroes to some dates
-#                          data.table = FALSE)
+fake <- data.table::fread("PreFer_fake_data.csv",
+                          keepLeadingZeros = TRUE, # if FALSE adds zeroes to some dates
+                          data.table = FALSE)
 
 predict_outcomes <- function(df, background_df = NULL, model_path = "./model.rds"){
   # Generate predictions using the saved model and the input dataframe.
@@ -554,4 +521,3 @@ predict_outcomes <- function(df, background_df = NULL, model_path = "./model.rds
 
 
 ###################################
-
