1fd1a5d21cdb0f9d0f21f75517ced86c84fb42d0
kqwright <kelsey.wright@helsinki.fi>
2024-09-16 11:12:09 +0300

    Update submission.R

--- submission.R
+++ submission.R
@@ -15,19 +15,6 @@ library(stats)
 #b. strongly advised to save datafiles separate from local repository so they are not accidently made public through a push function to GitHub
 #c. The code to read-in your data is the only code you don't need to document through your repository
 
-#data = read.csv('Z:/PreFer/training_data/PreFer_train_data.csv', 
- #                header=TRUE,sep=",")
-
-#outcome = read.csv('Z:/PreFer/training_data/PreFer_train_outcome.csv', 
- #                  header=TRUE,sep=",")
-
-#outcome <- data.table::fread("Z:/PreFer/training_data/PreFer_train_outcome.csv",
-#                           data.table = FALSE) 
-
-#train <- data.table::fread("Z:/PreFer/training_data/PreFer_train_data.csv",
- #                        keepLeadingZeros = TRUE, # if FALSE adds zeroes to some dates
-  #                       data.table = FALSE)
-
 
 #2. Preprocess data
 total <- merge(data,outcome,by="nomem_encr")
@@ -104,7 +91,7 @@ clean_df <- function(df, background_df = NULL) {
   # Get income, input missing values with mean income
   df$income <- df$nettohh_f_2020
   df[is.na(df$nettohh_f_2020),]$income <-  mean(df$nettohh_f_2020, na.rm=TRUE)
-  df$income_log <- df$log(income)
+  df[log(df$income),] <- df$income_log
   
   
   # Urban
@@ -425,47 +412,7 @@ clean_df <- function(df, background_df = NULL) {
 }
 
 
-
-#####################
-
-train_save_model <- function(cleaned_df, outcome_df) {
-  # Trains a model using the cleaned dataframe and saves the model to a file.
-  
-  # Parameters:
-  #cleaned_df (dataframe): The cleaned data from clean_df function to be used for training the model.
-  #outcome_df (dataframe): The data with the outcome variable (e.g., from PreFer_train_outcome.csv or PreFer_fake_outcome.csv).
-  
-  ## This script contains a bare minimum working example
-  #set.seed(1) # not useful here because logistic regression deterministic
-  
-  # Combine cleaned_df and outcome_df
-  model_df <- merge(cleaned_df, outcome_df, by = "nomem_encr")
-  
-  # Logistic regression model
-  model <- logistf (new_child ~ gender_bg*(age+migration+satisfied_own_finance+rela+satisfied+marriage_dur+owner+education+field_education+occupation+income+income_log+urban+urban_delta+health+gyno+relig_fa+sm_fa+dist_fr_parents+next_child+numb_child+first_birth+partner+partner_delta+partner_dur+partner_type+partner_type_delta+personality_factor1+personality_factor2+personality_factor3+personality_factor4+personality_factor5), data = model_df) 
-
-  # Save the model
-  saveRDS(model, "model.rds")
-}
-
-
-###############
-
-setwd("Z:/PreFer/") 
-
-train_cleaned <- clean_df(train)
-
-# training and saving the model
-train_save_model(train_cleaned, outcome)
-
-
 ###############################
-setwd("Z:/PreFer/other_data/") 
-
-
-fake <- data.table::fread("PreFer_fake_data.csv",
-                          keepLeadingZeros = TRUE, # if FALSE adds zeroes to some dates
-                          data.table = FALSE)
 
 predict_outcomes <- function(df, background_df = NULL, model_path = "./model.rds"){
   # Generate predictions using the saved model and the input dataframe.
