e98d2f4a840ec40916f6c88ad76868db1b86412e
kqwright <kelsey.wright@helsinki.fi>
2024-09-18 12:11:02 +0300

    Update submission.R

--- submission.R
+++ submission.R
@@ -202,6 +202,39 @@ clean_df <- function(df, background_df = NULL) {
 
 
 
+#####################
+
+#train_save_model <- function(cleaned_df, outcome_df) {
+# Trains a model using the cleaned dataframe and saves the model to a file.
+
+# Parameters:
+# cleaned_df (dataframe): The cleaned data from clean_df function to be used for training the model.
+# outcome_df (dataframe): The data with the outcome variable (e.g., from PreFer_train_outcome.csv or PreFer_fake_outcome.csv).
+
+## This script contains a bare minimum working example
+# set.seed(1) # not useful here because logistic regression deterministic
+
+# Combine cleaned_df and outcome_df
+#model_df <- merge(cleaned_df, outcome_df, by = "nomem_encr")
+
+# Logistic regression model
+#model <- glm(new_child ~ gender_bg*(age+migration+education+occupation+satisfied_own_finance+owner+income+rela_satisfied+health+relig+personality+urban+help_fr_parents+next_child+first_birth+partner_dur+marriage_dur+field_edu), family = "binomial", data = model_df) 
+
+# Save the model
+#saveRDS(model, "model.rds")
+#}
+
+
+###############
+
+#setwd("Z:/PreFer/") 
+
+#train_cleaned <- clean_df(train)
+
+# training and saving the model
+#train_save_model(train_cleaned, outcome)
+
+
 ###############################
 #setwd("Z:/PreFer/other_data/") 
 
