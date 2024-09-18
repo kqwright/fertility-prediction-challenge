e5c1ac91af0cd19777f397ec15e46880d5e00a41
kqwright <kelsey.wright@helsinki.fi>
2024-09-18 11:43:53 +0300

    Update training.R

--- training.R
+++ training.R
@@ -14,7 +14,7 @@ train_save_model <- function(cleaned_df, outcome_df) {
   
   # Logistic regression model
   #model <- logistf (new_child ~ gender_bg*(age+migration+education+field_edu+occupation+income+urban+owner+health+dist_fr_parents+next_child+first_birth+marriage_dur+partner_dur+rela_satisfied+relig1), data = model_df) 
-  model <- glm(new_child ~ gender_bg*(age+migration+education+occupation+satisfied_own_finance+owner+income+rela_satisfied+health+relig+personality+urban+help_fr_parents+next_child+first_birth+partner_dur+marriage_dur+field_edu), family = "binomial", data = model_df) 
+ model <- glm(new_child ~ gender_bg*(age+migration+education+occupation+owner+income+rela_satisfied+health+relig+urban+next_child+first_birth+partner_dur+marriage_dur+field_edu), family = "binomial", data = model_df) 
 
   # Save the model
   saveRDS(model, "model.rds")
