c7b9b19d3b48c53502e4e024e47a5cb68313f164
kqwright <kelsey.wright@helsinki.fi>
2024-09-18 10:22:48 +0300

    Update submission.R

--- submission.R
+++ submission.R
@@ -55,61 +55,67 @@ clean_df <- function(df, background_df = NULL) {
   df[df$migration_background_bg==202,]$migration <- "gen_2_non_west"
   
   # Get education level
+  df$oplcat_2020 [is.na(df$oplcat_2020)] <- 999 
   df$education <- NA
-  df[(df$oplmet_2020==1 | df$oplmet_2020==8 | df$oplmet_2020==9) & !is.na(df$oplmet_2020),]$education <- "1_prim_8_9"
-  df[df$oplmet_2020==2 & !is.na(df$oplmet_2020),]$education <- "2_sec_intermed"
-  df[df$oplmet_2020==3 & !is.na(df$oplmet_2020),]$education <- "3_sec_high"
-  df[df$oplmet_2020==4 & !is.na(df$oplmet_2020),]$education <- "4_voc_intermed"
-  df[df$oplmet_2020==5 & !is.na(df$oplmet_2020),]$education <- "5_voc_high"
-  df[df$oplmet_2020==6 & !is.na(df$oplmet_2020),]$education <- "6_uni"
-  df[df$oplmet_2020==7 | is.na(df$oplmet_2020),]$education <- "7_other_missing"
+  df[df$oplcat_2020==1 & !is.na(df$oplcat_2020),]$education <- "1_primary"
+  df[df$oplcat_2020==2 & !is.na(df$oplcat_2020),]$education <- "2_sec_intermed"
+  df[df$oplcat_2020==3 & !is.na(df$oplcat_2020),]$education <- "3_sec_senior"
+  df[df$oplcat_2020==4 & !is.na(df$oplcat_2020),]$education <- "4_voc_intermed"
+  df[df$oplcat_2020==5 & !is.na(df$oplcat_2020),]$education <- "5_voc_senior"
+  df[df$oplcat_2020==6 & !is.na(df$oplcat_2020),]$education <- "6_uni"
+  df[df$oplcat_2020==999 | is.na(df$oplcat_2020),]$education <- "other_missing"
+  
   
   # Get education field
-  df$field_edu <- "NA"
-  df[!is.na(df$cw20m011) & df$cw20m011==1,]$field_edu <- "general"
+  df$field_edu <- NA
+  df[!is.na(df$cw20m011) & df$cw20m011==1,]$field_edu <- "other"
   df[!is.na(df$cw20m012) & df$cw20m012==1,]$field_edu <- "teaching"
-  df[!is.na(df$cw20m013) & (df$cw20m013==1),]$field_edu <- "art_humanities"
-  df[!is.na(df$cw20m014) & (df$cw20m014==1),]$field_edu <- "art_humanities"
-  df[!is.na(df$cw20m015) & df$cw20m015==1,]$field_edu <- "social"
-  df[!is.na(df$cw20m016) & df$cw20m016==1,]$field_edu <- "economics"
-  df[!is.na(df$cw20m017) & df$cw20m017==1,]$field_edu <- "law"
-  df[!is.na(df$cw20m018) & df$cw20m018==1,]$field_edu <- "math"
-  df[!is.na(df$cw20m019) & (df$cw20m019==1),]$field_edu <- "technology_agri"
-  df[!is.na(df$cw20m020) & (df$cw20m020==1),]$field_edu <- "technology_agri"
-  df[!is.na(df$cw20m025) & (df$cw20m025==1),]$field_edu <- "technology_agri"
+  df[!is.na(df$cw20m013) & df$cw20m013==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m014) & df$cw20m014==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m015) & df$cw20m015==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m016) & df$cw20m016==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m017) & df$cw20m017==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m018) & df$cw20m018==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m019) & df$cw20m019==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m020) & df$cw20m020==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m025) & df$cw20m025==1,]$field_edu <- "other"
   df[!is.na(df$cw20m021) & df$cw20m021==1,]$field_edu <- "1_health"
-  df[!is.na(df$cw20m022) & df$cw20m022==1,]$field_edu <- "personal_care"
-  df[!is.na(df$cw20m023) & df$cw20m023==1,]$field_edu <- "catering"
-  df[!is.na(df$cw20m024) & (df$cw20m024==1),]$field_edu <- "transport_public_order"
-  df[!is.na(df$cw20m026) & (df$cw20m026==1),]$field_edu <- "transport_public_order"
+  df[!is.na(df$cw20m022) & df$cw20m022==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m023) & df$cw20m023==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m024) & df$cw20m024==1,]$field_edu <- "other"
+  df[!is.na(df$cw20m026) & df$cw20m026==1,]$field_edu <- "other"
   df[!is.na(df$cw20m027) & df$cw20m027==1,]$field_edu <- "other"
   
   # Get occupation
-  df$occupation <- "NA"
+    df$ci20m383[is.na(df$ci20m383)] <- 999
+  df$occupation <- NA
   df[!is.na(df$ci20m383) & (df$ci20m383==1 | df$ci20m383==2),]$occupation <- "employed"
   df[!is.na(df$ci20m383) & (df$ci20m383==3),]$occupation <- "self-employed"
   df[!is.na(df$ci20m383) & (df$ci20m383==7),]$occupation <- "student"
   df[!is.na(df$ci20m383) & (df$ci20m383==4 | df$ci20m383==5  | df$ci20m383==6 |df$ci20m383==8 |  df$ci20m383==9 | df$ci20m383==10 | df$ci20m383==11 | df$ci20m383==12 | df$ci20m383==13),]$occupation <- "not_employed"
+  df[!is.na(df$ci20m383) | (df$ci20m383==999),]$occupation <- "missing"
   
   # Get income, imput missing values with mean income
   df$income <- df$nettohh_f_2020
   df[is.na(df$nettohh_f_2020),]$income <-  mean(df$nettohh_f_2020, na.rm=TRUE)
   
   # Add how satisfied individuals are with their finances
-  df$satisfied_own_finance <- "NA"
-  df[!is.na(df$ci20m006) & (df$ci20m006==0 | df$ci20m006==1 | df$ci20m006==2 | df$ci20m006==3),]$satisfied_own_finance <- "0123_satisfied"
-  df[!is.na(df$ci20m006) & (df$ci20m006==4 | df$ci20m006==5),]$satisfied_own_finance <- "45_satisfied"
-  df[!is.na(df$ci20m006) & (df$ci20m006==6),]$satisfied_own_finance <- "6_satisfied"
-  df[!is.na(df$ci20m006) & (df$ci20m006==7),]$satisfied_own_finance <- "7_satisfied"
-  df[!is.na(df$ci20m006) & (df$ci20m006==8),]$satisfied_own_finance <- "8_satisfied"
-  df[!is.na(df$ci20m006) & (df$ci20m006==9 | df$ci20m006==10),]$satisfied_own_finance <- "910_satisfied"
+  #df$satisfied_own_finance <- "NA"
+  #df[!is.na(df$ci20m006) & (df$ci20m006==0 | df$ci20m006==1 | df$ci20m006==2 | df$ci20m006==3),]$satisfied_own_finance <- "0123_satisfied"
+  #df[!is.na(df$ci20m006) & (df$ci20m006==4 | df$ci20m006==5),]$satisfied_own_finance <- "45_satisfied"
+  #df[!is.na(df$ci20m006) & (df$ci20m006==6),]$satisfied_own_finance <- "6_satisfied"
+  #df[!is.na(df$ci20m006) & (df$ci20m006==7),]$satisfied_own_finance <- "7_satisfied"
+  #df[!is.na(df$ci20m006) & (df$ci20m006==8),]$satisfied_own_finance <- "8_satisfied"
+  #df[!is.na(df$ci20m006) & (df$ci20m006==9 | df$ci20m006==10),]$satisfied_own_finance <- "910_satisfied"
   
   # Add whether the respondent is an owner of its current dwelling
   df$owner <- "NA"
   df[!is.na(df$cd20m003) & (df$cd20m003==1 |df$cd20m003==2 | df$cd20m003==4),]$owner <- "1_no"
   df[!is.na(df$cd20m003) & df$cd20m003==3,]$owner <- "2_yes"
   
-  # Generate relationship satisfaction
+ # Generate relationship satisfaction
+  df$cf20m180[is.na(df$cf20m180)] <-999
+  df$cf20m166[is.na(df$cf20m166)] <-999
   df$rela_satisfied <- "8_NA"
   df[!is.na(df$cf20m166) & df$cf20m166<=4,]$rela_satisfied <- "7_single_dissatisfied"
   df[!is.na(df$cf20m166) & df$cf20m166>=5 & df$cf20m166<=7,]$rela_satisfied <- "6_single_neutral"
@@ -117,45 +123,54 @@ clean_df <- function(df, background_df = NULL) {
   df[!is.na(df$cf20m180) & df$cf20m180<=6,]$rela_satisfied <- "3_relation_less_satisfied"
   df[!is.na(df$cf20m180) & df$cf20m180>=7 & df$cf20m180<=8,]$rela_satisfied <- "2_relation_satisfied"
   df[!is.na(df$cf20m180) & df$cf20m180>=9,]$rela_satisfied <- "1_relation_very_satisfied"
+  df[!is.na(df$cf20m166) | df$cf20m166==999, !is.na(df$cf20m180) | df$cf20m180==999]$rela_satisfied  <- "missing"
+  
   
   # Generate health
+  df$ch20m004[is.na(df$ch20m004 )] <- 999
   df$health <- "NA"
   df[!is.na(df$ch20m004) & (df$ch20m004==1 | df$ch20m004==2),]$health <- "12_poor_moderate"
   df[!is.na(df$ch20m004) & df$ch20m004==3,]$health <- "3_good"
   df[!is.na(df$ch20m004) & df$ch20m004==4,]$health <- "4_very_good"
   df[!is.na(df$ch20m004) & df$ch20m004==5,]$health <- "5_excellent"
+  df[!is.na(df$ch20m004) | df$ch20m004==999,]$health <- "missing"
   
   # Generate religiousness
+  df$cr20m041[is.na(df$cr20m041)] <-999
   df$relig <- "5_NA"
   df[!is.na(df$cr20m041) & (df$cr20m041==1 | df$cr20m041==2 | df$cr20m041==3) ,]$relig <- "4_>once_week"
   df[!is.na(df$cr20m041) & (df$cr20m041==4) ,]$relig <- ">3_once_month"
   df[!is.na(df$cr20m041) & (df$cr20m041==5) ,]$relig <- ">2_few_per_year"
   df[!is.na(df$cr20m041) & (df$cr20m041==6) ,]$relig <- "1_never"
+  df[!is.na(df$cr20m041) | df$cr20m041==999,]$relig1 <- "missing"
   
   # Generate personality
   #df[is.na(df$cp20l026),]$cp20l026 <- "999"
   #df[is.na(df$cp20l023),]$cp20l023 <- "999"
-  df$personality <- "NA"
-  df[!is.na(df$cp20l026) & !is.na(df$cp20l023) & (df$cp20l026==1 | df$cp20l026==2 | df$cp20l026==3) & (df$cp20l023==1 | df$cp20l023==2 | df$cp20l023==3),]$personality <- "less_social_less_stressed"
-  df[!is.na(df$cp20l026) & !is.na(df$cp20l023) & (df$cp20l026==4 | df$cp20l026==5) & (df$cp20l023==1 | df$cp20l023==2 | df$cp20l023==3),]$personality <- "more_social_less_stressed"
-  df[!is.na(df$cp20l026) & !is.na(df$cp20l023) & (df$cp20l026==1 | df$cp20l026==2 | df$cp20l026==3) & (df$cp20l023==4 | df$cp20l023==5),]$personality <- "less_social_more_stressed"
-  df[!is.na(df$cp20l026) & !is.na(df$cp20l023) & (df$cp20l026==4 | df$cp20l026==5) & (df$cp20l023==4 | df$cp20l023==5),]$personality <- "more_social_more_stressed"
+  #df$personality <- "NA"
+  #df[!is.na(df$cp20l026) & !is.na(df$cp20l023) & (df$cp20l026==1 | df$cp20l026==2 | df$cp20l026==3) & (df$cp20l023==1 | df$cp20l023==2 | df$cp20l023==3),]$personality <- "less_social_less_stressed"
+  #df[!is.na(df$cp20l026) & !is.na(df$cp20l023) & (df$cp20l026==4 | df$cp20l026==5) & (df$cp20l023==1 | df$cp20l023==2 | df$cp20l023==3),]$personality <- "more_social_less_stressed"
+  #df[!is.na(df$cp20l026) & !is.na(df$cp20l023) & (df$cp20l026==1 | df$cp20l026==2 | df$cp20l026==3) & (df$cp20l023==4 | df$cp20l023==5),]$personality <- "less_social_more_stressed"
+  #df[!is.na(df$cp20l026) & !is.na(df$cp20l023) & (df$cp20l026==4 | df$cp20l026==5) & (df$cp20l023==4 | df$cp20l023==5),]$personality <- "more_social_more_stressed"
   
   # Generate living arrangement
-  df$urban <- "6_NA"
-  df[!is.na(df$sted_2020) & df$sted_2020==1,]$urban <- "1_extreme_urban"
-  df[!is.na(df$sted_2020) & df$sted_2020==2,]$urban <- "2_very_urban"
-  df[!is.na(df$sted_2020) & df$sted_2020==3,]$urban <- "3_mod_urban"
-  df[!is.na(df$sted_2020) & df$sted_2020==4,]$urban <- "4_slight_urban"
-  df[!is.na(df$sted_2020) & df$sted_2020==5,]$urban <- "5_not_urban"
+  df$sted_2020[is.na(df$sted_2020)] <- 999  
+  df$sted_2019[is.na(df$sted_2019)] <- 999  
+  df$urban <- "NA"
+  df[!is.na(df$sted_2020) & df$sted_2020==1,]$urban <- "1_extremely urban"
+  df[!is.na(df$sted_2020) & df$sted_2020==2,]$urban <- "2_very urban"
+  df[!is.na(df$sted_2020) & df$sted_2020==3,]$urban <- "3_moderately urban"
+  df[!is.na(df$sted_2020) & df$sted_2020==4,]$urban <- "4_slightly urban"
+  df[!is.na(df$sted_2020) & df$sted_2020==5,]$urban <- "5_slightly urban"
+  df[!is.na(df$sted_2020) | df$sted_2020==999,]$urban <- "missing"
   
   # Generate help from parents
-  df[is.na(df$cf20m134),]$cf20m134 <- "999"
-  df[is.na(df$cf20m133),]$cf20m133 <- "999"
-  df$help_fr_parents <- "NA"
-  df[(df$cf20m134==3 | df$cf20m133==3),]$help_fr_parents <- "several_times"
-  df[(df$cf20m134==2 | df$cf20m133==2) & (df$cf20m134!=3 & df$cf20m133!=3),]$help_fr_parents <- "once_twice"
-  df[(df$cf20m134==1 | df$cf20m133==1) & (df$cf20m134!=2 & df$cf20m133!=2 & df$cf20m134!=3 & df$cf20m133!=3),]$help_fr_parents <- "never"
+  #df[is.na(df$cf20m134),]$cf20m134 <- "999"
+  #df[is.na(df$cf20m133),]$cf20m133 <- "999"
+  #df$help_fr_parents <- "NA"
+  #df[(df$cf20m134==3 | df$cf20m133==3),]$help_fr_parents <- "several_times"
+  #df[(df$cf20m134==2 | df$cf20m133==2) & (df$cf20m134!=3 & df$cf20m133!=3),]$help_fr_parents <- "once_twice"
+  #df[(df$cf20m134==1 | df$cf20m133==1) & (df$cf20m134!=2 & df$cf20m133!=2 & df$cf20m134!=3 & df$cf20m133!=3),]$help_fr_parents <- "never"
   
   # Generate fertility intentions 
   df$cf20m130[is.na(df$cf20m130)] <- 999
@@ -164,7 +179,7 @@ clean_df <- function(df, background_df = NULL) {
   df[df$cf20m130<=1,]$next_child <- "<1_year"
   df[df$cf20m130>=2 & df$cf20m130<=3,]$next_child <- "2_3_year"
   df[df$cf20m130>=4,]$next_child <- ">4_year"
-  df[df$cf20m130==999,]$next_child <- "NA"
+  df[df$cf20m130==999,]$next_child <- "missing"
   df[df$cf20m128==2,]$next_child <- "no"
   
   # First birth
@@ -207,12 +222,10 @@ clean_df <- function(df, background_df = NULL) {
                'field_edu',
                'occupation',
                'income',
-               'satisfied_own_finance',
                'owner',
                'rela_satisfied',
                'health',
                'relig',
-               'personality',
                'urban',
                'help_fr_parents',
                'next_child',
